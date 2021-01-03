987f48a06a72aa95444598d6a52d96476db71c8d
gildor <73232606+remote-gildor@users.noreply.github.com>
2021-01-03 23:18:29 +0100

    a better address presentation in navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -10,10 +10,14 @@
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav class="ml-auto">
         <router-link to="/set-value"><b-nav-item href="/set-value">Set value</b-nav-item></router-link>
-        <router-link to="/profile"><b-nav-item href="/profile">Profile</b-nav-item></router-link>
+        <router-link to="/profile"><b-nav-item href="/profile">
+          Profile 
+          <span v-if="isUserConnected">
+            <small><em>({{getActiveAccount.substring(0, 7)}}...)</em></small>
+          </span>
+        </b-nav-item></router-link>
 
         <b-nav-item v-if="!isUserConnected" @click="login">Connect your wallet</b-nav-item>
-        <b-nav-item v-if="isUserConnected">{{getActiveAccount}}</b-nav-item>
         <b-nav-item v-if="isUserConnected" @click="logout">Disconnect</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
@@ -27,7 +31,7 @@ import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Navbar",
   computed: {
-    ...mapGetters("accounts", ["getActiveAccount", "isUserConnected", "getWeb3Modal", "getWeb3"]),
+    ...mapGetters("accounts", ["getActiveAccount", "isUserConnected", "getWeb3Modal"]),
   },
   created() {
     this.$store.dispatch("accounts/initWeb3Modal");
