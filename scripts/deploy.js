114e7f9b984676a368634a5d7eb7d9cb3f8a1c26
gildor <73232606+remote-gildor@users.noreply.github.com>
2021-01-06 21:13:40 +0100

    store multiple addresses based on chain ID and contract name

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,31 +2,51 @@ const fs = require('fs');
 const path = require('path');
 
 async function main() {
+  let contractName = "Calc";
 
-    const [deployer] = await ethers.getSigners();
+  const [deployer] = await ethers.getSigners();
+
+  let networkData = await deployer.provider.getNetwork()
+  console.log("Chain ID:", networkData.chainId);
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
   
-    console.log(
-      "Deploying contracts with the account:",
-      deployer.address
-    );
-    
-    console.log("Account balance:", (await deployer.getBalance()).toString());
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // deploy the contract
+  const contractFactory = await ethers.getContractFactory(contractName);
+  const contract = await contractFactory.deploy();
+
+  console.log("Contract address:", contract.address);
   
-    const contractFactory = await ethers.getContractFactory("Calc");
-    const contract = await contractFactory.deploy();
+  // copy the contract JSON file to front-end and add the address field in it
+  fs.copyFileSync(
+    path.join(__dirname, "../artifacts/contracts/" + contractName + ".sol/" + contractName + ".json"), //source
+    path.join(__dirname, "../frontend/src/contracts/" + contractName + ".json") // destination
+  );
+
+  // update the addresses.json file with the new contract address
+  let addressesFile = fs.readFileSync(path.join(__dirname, "../frontend/src/contracts/addresses.json"));
+  let addressesJson = JSON.parse(addressesFile);
   
-    console.log("Contract address:", contract.address);
-    
-    // copy the contract JSON file to front-end and add the address field in it
-    let contractJsonFile = fs.readFileSync(path.join(__dirname, "../artifacts/contracts/Calc.sol/Calc.json"));
-    let contractJson = JSON.parse(contractJsonFile);
-    contractJson.address = contract.address; // add address to contract JSON
-    fs.writeFileSync(path.join(__dirname, "../frontend/src/contracts/Calc.json"), JSON.stringify(contractJson)); 
+  if (!addressesJson[contractName]) {
+    addressesJson[contractName] = {};
   }
   
-  main()
-    .then(() => process.exit(0))
-    .catch(error => {
-      console.error(error);
-      process.exit(1);
-    });
+  addressesJson[contractName][networkData.chainId] = contract.address;
+
+  fs.writeFileSync(
+    path.join(__dirname, "../frontend/src/contracts/addresses.json"), 
+    JSON.stringify(addressesJson)
+  ); 
+}
+  
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
