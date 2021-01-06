56688ea6c1db85f06c9060a7b534db8846cf04ac
gildor <73232606+remote-gildor@users.noreply.github.com>
2021-01-06 21:39:25 +0100

    deploy.js: support for multiple contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,52 @@
 const fs = require('fs');
 const path = require('path');
 
-async function main() {
-  let contractName = "Calc";
+// write down contracts that you wish to deploy one-by-one (names only, no .sol extension)
+// after the run, find the ABIs and addresses in frontend/src/contracts
+const contracts = ["Calc"];
 
+// DO NOT MODIFY CODE BELOW UNLESS ABSOLUTELY NECESSARY
+async function publishContract(contractName, chainId) {
+  // deploy the contract
+  const contractFactory = await ethers.getContractFactory(contractName);
+  const contract = await contractFactory.deploy();
+
+  console.log(contractName + " contract address: " + contract.address);
+
+  // copy the contract JSON file to front-end and add the address field in it
+  fs.copyFileSync(
+    path.join(__dirname, "../artifacts/contracts/" + contractName + ".sol/" + contractName + ".json"), //source
+    path.join(__dirname, "../frontend/src/contracts/" + contractName + ".json") // destination
+  );
+
+  // check if addresses.json already exists
+  let exists = fs.existsSync(path.join(__dirname, "../frontend/src/contracts/addresses.json"));
+
+  // if not, created the file
+  if (!exists) {
+    fs.writeFileSync(
+      path.join(__dirname, "../frontend/src/contracts/addresses.json"), 
+      "{}"
+    ); 
+  }
+
+  // update the addresses.json file with the new contract address
+  let addressesFile = fs.readFileSync(path.join(__dirname, "../frontend/src/contracts/addresses.json"));
+  let addressesJson = JSON.parse(addressesFile);
+
+  if (!addressesJson[contractName]) {
+    addressesJson[contractName] = {};
+  }
+
+  addressesJson[contractName][chainId] = contract.address;
+
+  fs.writeFileSync(
+    path.join(__dirname, "../frontend/src/contracts/addresses.json"), 
+    JSON.stringify(addressesJson)
+  ); 
+}
+
+async function main() {
   const [deployer] = await ethers.getSigners();
 
   let networkData = await deployer.provider.getNetwork()
@@ -16,32 +59,9 @@ async function main() {
   
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  // deploy the contract
-  const contractFactory = await ethers.getContractFactory(contractName);
-  const contract = await contractFactory.deploy();
-
-  console.log("Contract address:", contract.address);
-  
-  // copy the contract JSON file to front-end and add the address field in it
-  fs.copyFileSync(
-    path.join(__dirname, "../artifacts/contracts/" + contractName + ".sol/" + contractName + ".json"), //source
-    path.join(__dirname, "../frontend/src/contracts/" + contractName + ".json") // destination
-  );
-
-  // update the addresses.json file with the new contract address
-  let addressesFile = fs.readFileSync(path.join(__dirname, "../frontend/src/contracts/addresses.json"));
-  let addressesJson = JSON.parse(addressesFile);
-  
-  if (!addressesJson[contractName]) {
-    addressesJson[contractName] = {};
+  for (cont of contracts) {
+    await publishContract(cont, networkData.chainId);
   }
-  
-  addressesJson[contractName][networkData.chainId] = contract.address;
-
-  fs.writeFileSync(
-    path.join(__dirname, "../frontend/src/contracts/addresses.json"), 
-    JSON.stringify(addressesJson)
-  ); 
 }
   
 main()
